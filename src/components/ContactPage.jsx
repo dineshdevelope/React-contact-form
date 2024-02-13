c1bc88789a92d4e3ca35b35340eea7bd38f16545
dineshdevelope <dineshindevelope@gmail.com>
2024-02-14 00:05:58 +0530

    validation

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import Forminput from "./Forminput";
 import FormTextArea from "./FormtextArea";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  fullName: z.string().min(3).max(20),
+  email: z.string().email(),
+  subject: z.string().min(10).max(150),
+  desc: z.string().min(50).max(500),
+});
 
 const contactPage = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
   //console.log(register);
   const sendInfo = (data) => {
     console.log(data);
